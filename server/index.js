8ffca290a00a02dd0ba71a37f102a324b50ed2f4
Danara Tagayeva <danara.tagayeva@gmail.com>
2022-11-02 17:46:27 -0400

    setting up backend via express,mongoose

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log('Connected to DB!');
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
 app.listen(8800, () => {
-  console.log('Connected!');
+  connect();
+  console.log('Connected to Server!');
 });
