05ab9a539b6a56e3bdef7ae350e7c0808f546fd6
Danara Tagayeva <danara.tagayeva@gmail.com>
2022-11-04 23:48:46 -0400

    in progress

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,27 +1,29 @@
-export const update = (req, res,next) => {
-  res.json('success user');
+import { createError } from "../error";
+import User from "../models/User";
+
+export const update = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+        $set: res.body,
+      });
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    return next(createError(403, "You can update only own account!"));
+  }
 };
 
-export const delete = (req,res,next)=>{
+export const deleteUser = (req, res, next) => {};
 
-}
+export const getUser = (req, res, next) => {};
 
-export const getUser=(req,res,next)=>[
+export const subscribe = (req, res, next) => {};
 
-]
+export const unsubscribe = (req, res, next) => {};
 
-export const subscribe=(req,res,next)=>{
+export const like = (req, res, next) => {};
 
-}
-
-export const unsubscribe=(req,res,next)=>{
-
-}
-
-export const like=(req,res,next)=>{
-
-}
-
-export const dislike=(req,res,next)=>{
-  
-}
+export const dislike = (req, res, next) => {};
