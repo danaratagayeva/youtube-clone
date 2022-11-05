6fad35bda1175a513d0da9c015c82f4e628b7907
Danara Tagayeva <danara.tagayeva@gmail.com>
2022-11-04 23:28:27 -0400

    users.js toue in work

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,27 @@
-export const test = (req, res) => {
+export const update = (req, res,next) => {
   res.json('success user');
 };
+
+export const delete = (req,res,next)=>{
+
+}
+
+export const getUser=(req,res,next)=>[
+
+]
+
+export const subscribe=(req,res,next)=>{
+
+}
+
+export const unsubscribe=(req,res,next)=>{
+
+}
+
+export const like=(req,res,next)=>{
+
+}
+
+export const dislike=(req,res,next)=>{
+  
+}
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,21 @@
-import express from 'express';
-import {} from '../controllers/user.js';
+import express from "express";
+import {} from "../controllers/user.js";
 
 const router = express.Router();
+
+//update user
+router.put("/:id");
+
+//delete user
+
+//get a user
+
+//subscribe a user
+
+//unsubscribe a user
+
+//like a video
+
+//dislike a video
 
 export default router;
