8be16aac2c23dff2e0d23d49e27c758295e02cbe
Danara Tagayeva <danara.tagayeva@gmail.com>
2022-11-05 22:49:44 -0400

    delete user and get user features added

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,5 @@
-import { createError } from "../error.js";
-import User from "../models/User.js";
+import { createError } from '../error.js'
+import User from '../models/User.js'
 
 export const update = async (req, res, next) => {
   if (req.params.id === req.user.id) {
@@ -12,24 +12,62 @@ export const update = async (req, res, next) => {
         {
           new: true,
         }
-      );
-      res.status(200).json(updatedUser);
+      )
+      res.status(200).json(updatedUser)
     } catch (err) {
-      next(err);
+      next(err)
     }
   } else {
-    return next(createError(403, "You can update only own account!"));
+    return next(createError(403, 'You can update only own account!'))
   }
-};
+}
 
-export const deleteUser = (req, res, next) => {};
+export const deleteUser = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      await User.findByIdAndDelete(req.params.id)
+      res.status(200).json('User has been deleted.')
+    } catch (err) {
+      next(err)
+    }
+  } else {
+    return next(createError(403, 'You can delete only your account!'))
+  }
+}
 
-export const getUser = (req, res, next) => {};
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req, params.id)
+    res.status(200).json(user)
+  } catch (err) {
+    next(err)
+  }
+}
 
-export const subscribe = (req, res, next) => {};
+export const subscribe = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err)
+  }
+}
 
-export const unsubscribe = (req, res, next) => {};
+export const unsubscribe = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err)
+  }
+}
 
-export const like = (req, res, next) => {};
+export const like = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err)
+  }
+}
 
-export const dislike = (req, res, next) => {};
+export const dislike = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err)
+  }
+}
