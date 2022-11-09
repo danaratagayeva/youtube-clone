b4e869831446347d20a02fedb225f03909909d60
Danara Tagayeva <danara.tagayeva@gmail.com>
2022-11-08 23:42:23 -0500

    added getvideos response to the home component via axios

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,30 +1,34 @@
-import React from "react";
-import styled from "styled-components";
-import Card from "../components/Card";
+import React from 'react'
+import styled from 'styled-components'
+import Card from '../components/Card'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 const Container = styled.div`
   display: flex;
   justify-content: space-between;
   flex-wrap: wrap;
-`;
+`
 
 const Home = () => {
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      const res = await axios.get('/videos/random')
+      console.log(res.data)
+      setVideos(res.data)
+    }
+    fetchVideos()
+  }, [])
+
   return (
     <Container>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {videos.map((video) => (
+        <Card video={video} key={video._id} />
+      ))}
     </Container>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
