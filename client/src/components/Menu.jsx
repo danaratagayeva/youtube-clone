6c3d6ce45e38b3dfd5f70c95278f8e88ffdc7f40
Danara Tagayeva <danara.tagayeva@gmail.com>
2022-11-13 23:55:19 -0500

    redux persist methods used, to be logged by user after refreshing the page

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -18,6 +18,7 @@ import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined'
 import SettingsBrightnessOutlinedIcon from '@mui/icons-material/SettingsBrightnessOutlined'
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined'
 import SignIn from '../pages/SignIn'
+import { useSelector } from 'react-redux'
 
 const Container = styled.div`
   flex: 1;
@@ -85,6 +86,7 @@ const Title = styled.h2`
 
 const Menu = (props) => {
   const { darkMode, setDarkMode } = props
+  const { currentUser } = useSelector((state) => state.user)
   return (
     <Container>
       <Wrapper>
@@ -123,16 +125,20 @@ const Menu = (props) => {
           History
         </Item>
         <Hr />
-        <Login>
-          Sign in to like videos, comment and subscribe.
-          <Link to="signin" style={{ textDecoration: 'none' }}>
-            <Button>
-              <AccountCircleOutlinedIcon />
-              SIGN IN
-            </Button>
-          </Link>
-        </Login>
-        <Hr />
+        {!currentUser && (
+          <>
+            <Login>
+              Sign in to like videos, comment and subscribe.
+              <Link to="signin" style={{ textDecoration: 'none' }}>
+                <Button>
+                  <AccountCircleOutlinedIcon />
+                  SIGN IN
+                </Button>
+              </Link>
+            </Login>
+            <Hr />
+          </>
+        )}
         <Title>BEST OF YOU-CLONE</Title>
         <Item>
           <LibraryMusicOutlinedIcon />
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-import SignIn from '../pages/SignIn';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined'
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined'
+import VideoCallOutlinedIcon from '@mui/icons-material/VideoCallOutlined'
+//import SignIn from '../pages/SignIn'
+import { useSelector } from 'react-redux'
+//import { Avatar } from '@mui/material'
 
 const Container = styled.div`
   position: sticky;
   top: 0;
   background-color: ${({ theme }) => theme.bgLighter};
   height: 56px;
-`;
+`
 
 const Wrapper = styled.div`
   display: flex;
@@ -19,7 +22,22 @@ const Wrapper = styled.div`
   height: 100%;
   padding: 0px 20px;
   position: relative;
-`;
+`
+
+const User = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text};
+`
+
+const Avatar = styled.div`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  background-color: #999;
+`
 
 const Search = styled.div`
   width: 40%;
@@ -33,12 +51,12 @@ const Search = styled.div`
   padding: 5px;
   border: 1px solid #cccccc;
   border-radius: 3px;
-`;
+`
 
 const Input = styled.input`
   border: none;
   background-color: transparent;
-`;
+`
 
 const Button = styled.button`
   padding: 5px 15px;
@@ -51,25 +69,35 @@ const Button = styled.button`
   display: flex;
   align-items: center;
   gap: 5px;
-`;
+`
 
 const Navbar = () => {
+  const { currentUser } = useSelector((state) => state.user)
+
   return (
     <Container>
       <Wrapper>
         <Search>
-          <Input placeholder='Search' />
+          <Input placeholder="Search" />
           <SearchOutlinedIcon />
         </Search>
-        <Link to='signin' style={{ TextDecoration: 'none' }}>
-          <Button>
-            <AccountCircleOutlinedIcon />
-            SIGN IN
-          </Button>
-        </Link>
+        {currentUser ? (
+          <User>
+            <VideoCallOutlinedIcon />
+            <Avatar />
+            {currentUser.name}
+          </User>
+        ) : (
+          <Link to="signin" style={{ TextDecoration: 'none' }}>
+            <Button>
+              <AccountCircleOutlinedIcon />
+              SIGN IN
+            </Button>
+          </Link>
+        )}
       </Wrapper>
     </Container>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
